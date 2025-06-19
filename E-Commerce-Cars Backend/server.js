0ecfa010d169c3bab301fb7a47390bdb0baf4c93
thelalem <thelalemarg@gmail.com>
2025-06-19 19:59:36 +0300

    Integration v6.2

--- E-Commerce-Cars Backend/server.js
+++ E-Commerce-Cars Backend/server.js
@@ -1,11 +1,18 @@
 import http from 'http';
 import app from './app.js';
 import dotenv from 'dotenv';
+import express from 'express';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
+const path = require('path');
+
+server.use(express.static(path.join(__dirname, 'client/build')));
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
