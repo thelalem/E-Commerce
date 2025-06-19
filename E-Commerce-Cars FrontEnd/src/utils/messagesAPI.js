5055bcb00171469de757f061782a4838e13ceafa
Zelalem Argaw <thelalemarg@gmail.com>
2025-06-19 20:34:50 +0300

    Delete E-Commerce-Cars FrontEnd directory

--- E-Commerce-Cars FrontEnd/src/utils/messagesAPI.js
+++
@@ -1,9 +0,0 @@
-import axiosClient from "./axios";
-
-export const getMessages = async (chatId) => {
-    return await axiosClient.get('/messages');
-}
-
-export const sendMessage = async (message) => {
-    return await axiosClient.post("/messages", message);
-}
