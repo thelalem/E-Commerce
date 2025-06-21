d8fd0272e7c83a70fad0e2fd9ee87f882466f7f0
thelalem <thelalemarg@gmail.com>
2025-06-21 03:03:08 +0300

    deployment

--- E-Commerce-Cars Backend/app.js
+++ E-Commerce-Cars Backend/app.js
@@ -39,7 +39,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(generalRateLimiter);
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL,
     credentials: true,
 }));
 
